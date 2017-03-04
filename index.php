ba9317dacc0bcb9c1b3f5ac119bae0b9124e6f3e
Gina Levy <ginalevy@knights.ucf.edu>
2017-03-04 11:51:07 -0500

    adding an about it section as an article.

--- index.php
+++ index.php
@@ -22,4 +22,31 @@
 		</ul></nav>
 	</header>
 
+<section id="content" class="body">
+	  
+	  <article class="hentry">	
+			<header>
+				<h2 class="entry-title"><a href="#" rel="bookmark" title="Skimmer Alert!">Alerting the Community of Where they Detected a Skimmer</a></h2>
+			</header>
+			
+			<footer class="post-info">
+				<abbr class="published" title="2017-04-10T14:07:00-07:00">
+					4th March 2017
+				</abbr>
+
+				<address class="vcard author">
+					By <a class="url fn" href="#">Gina M. Levy</a>
+				</address>
+			</footer>
+			
+			<div class="entry-content">
+				<p>There has been an increase in number of people getting their credit cards number digitally stolen over the years. 
+				This is done by skimmers that are commonly found in gas stations, stores, and in other sketchy areas.
+				By banning together as a community we can voice our concerns and post where we have been last "hacked" 
+				to help others avoid the same area and let the proper authorities know about it as well.
+				</p>
+			</div>
+		</article>
+			
+	</section>
     
