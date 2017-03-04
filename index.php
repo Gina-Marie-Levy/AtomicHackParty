03552ba6120cd9caaad8db51dcd58129b6fa4802
Gina Levy <ginalevy@knights.ucf.edu>
2017-03-04 11:24:39 -0500

    the nav bar section...some of it might be changes. or maybe just one page?

--- index.php
+++ index.php
@@ -8,3 +8,18 @@
 	<meta charset="utf-8" />
 	
 	<link rel="stylesheet" href="css/main.css" type="text/css" />
+
+</head>
+
+<body id="index" class="home">
+
+<header id="banner" class="body">
+		<h1><a href="#">Alerting people where they found a skimmer. </a></h1>
+		<nav><ul>
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#">Live Chat</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul></nav>
+	</header>
+
+    
