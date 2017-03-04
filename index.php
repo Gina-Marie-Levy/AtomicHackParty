d83203e00427c817eb8369c20ffe494b897066bd
Gina Levy <ginalevy@knights.ucf.edu>
2017-03-04 11:18:56 -0500

    Added css folder and main.css. Also added more to index.

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<php
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Skimmer Alert!</title>
+	<meta charset="utf-8" />
+	
+	<link rel="stylesheet" href="css/main.css" type="text/css" />
